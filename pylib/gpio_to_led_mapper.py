0967d45979d5c176c553be0febb221aa8333ad89
DFriend01 <devon.fri.2000@gmail.com>
2021-12-20 11:55:58 -0800

    Added documentation to pylib

--- pylib/gpio_to_led_mapper.py
+++ pylib/gpio_to_led_mapper.py
@@ -2,7 +2,6 @@ from .receiver import Receiver
 from gpiozero import LED
 
 MAX_LEDS = 8
-BASE16 = 16
 
 def getDataAsInt(data):
     dec_val = int(data)
@@ -10,6 +9,31 @@ def getDataAsInt(data):
 
 class GpioToLedMapper:
     def __init__(self, *leds, transmitterName, baudrate=9600, timeout=1):
+        """
+        Initializes a GpioToLedMapper object used to map GPIO pins on the raspberry pi
+        to LEDs. This class also handles updating the LED configuration when it is changed.
+
+        Arguments
+        ---------
+        *leds
+            This variadic argument accepts integers that represent the GPIO pins that are
+            being mapped to LEDs. Visit https://pinout.xyz/# to learn about the GPIO pinouts.
+            The LSB of the data received by this object represents the status (0 - off, 1- on) 
+            of the first GPIO pin passed in this argument, and the MSB for the last argument.
+
+            Note: A maximum of 8 GPIO pins can be mapped using this class. Any pins exceeding
+                  the maximum allowable threshold are discarded.
+
+        str : transmitterName
+            The name of the USB communication device that acts as the transmitter.
+
+        int : baudrate (default: 9600)
+            The baudrate of the receiving device. Both the transmitter and the receiver
+            must run at the same baudrate.
+
+        float : timeout (default: 1)
+            Set a read timeout value in seconds.
+        """
         led_pins = leds if len(leds) <= MAX_LEDS else leds[:MAX_LEDS]
         self.__leds = []
         for i in range(len(led_pins)):
@@ -18,9 +42,12 @@ class GpioToLedMapper:
         self.__numLeds = len(self.__leds)
         self.__led_config = [0] * self.__numLeds
         self.__receiver = Receiver(transmitterName, baudrate, timeout, getDataAsInt)
-        
 
     def updateLeds(self):
+        """
+        Waits for new data and updates the LED configuration upon receiving data.
+
+        """
         new_data = self.__receiver.spin_for_data()
         self.__getNewLedConfig(new_data)
         self.__lightLeds()
--- pylib/receiver.py
+++ pylib/receiver.py
@@ -2,19 +2,67 @@ import serial
 
 class Receiver:
     def __init__(self, transmitterName, baudrate=9600, timeout=1, callback=lambda x: print(x + "\n")):
-        self.__transmitterName = transmitterName
-        self.__baudrate = baudrate
-        self.__timeout = timeout
-        self.__callback = callback
+        """
+        Initializes a Receiver object and is used to communicate with a transmitting device and
+        handle new data upon receiving it.
 
+        Arguments
+        ---------
+        str : transmitterName
+            The name of the USB communication device that acts as the transmitter.
+
+        int : baudrate (default: 9600)
+            The baudrate of the receiving device. Both the transmitter and the receiver
+            must run at the same baudrate.
+
+        float : timeout (default: 1)
+            Set a read timeout value in seconds.
+
+        callable : callback (default: print)
+            Set a callback function that is invoked upon receiving data. The callback
+            function must reserve the first parameter for the incoming data. By default,
+            the data received is printed to the terminal.
+        
+        """
+        self.__callback = callback
         self.__ser = serial.Serial(transmitterName, baudrate, timeout=timeout)
         self.__ser.reset_input_buffer()
+        self.__latestData = ""
 
     def spin_for_data(self):
+        """
+        Busy waits for data to be received. Upon receiving data, the user-specified callback
+        is invoked.
+
+        Modifies
+        --------
+        __latestData
+            Updates the latest data read.
+
+        Returns
+        -------
+        Returns the value, if any, that is retured from the user-specified callback.
+
+        """
         while True:
             if self.__ser.in_waiting > 0:
                 data = self.__ser.readline().decode("utf-8").rstrip()
+                self.__latestData = data
                 return self.__invoke_callback(data)
 
+    def get_latest_data(self):
+        """
+        Returns
+        -------
+        str
+            Returns a string containing the latest data read.
+
+        """
+        return self.__latestData
+
     def __invoke_callback(self, data):
+        """
+        Invoke the callback and return its value, if any.
+
+        """
         return self.__callback(data)
